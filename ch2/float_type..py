60b084a36d1aa03c9c0cdc63f8b04197485aa190
gre888 <gregary8@gmail.com>
2026-07-21 16:14:26 +0800

    20260721

---
+++ ch2/float_type..py
@@ -0,0 +1,9 @@
+f, i =1.2345, 12345
+print(type(f))
+f2=float(i)
+print(f2)
+print(float.is_integer(f))
+print(float.is_integer(f2))
+print(round(f, 2))
+print(round(f))
+print(isinstance(f2,int))
---
+++ ch2/variable.py
@@ -0,0 +1,12 @@
+yesNo='y'
+pssScore, maxScore, minSore = 85, 10, 0
+power1 = power2 = 100
+total=1.23456e+6 #123456.0
+print(yesNo)
+print(pssScore, maxScore, minSore)
+print(power1, power2)
+print(total)
+del total
+print(total) #報錯 已經刪除
+
+
