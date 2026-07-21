2b06a6f56aa305cdf3d9e3d0a04ab6efd9116887
gre888 <gregary8@gmail.com>
2026-07-21 11:24:26 +0800

    first commit

---
+++ ch2/int.py
@@ -0,0 +1,6 @@
+print(12) #10
+print(0b1100)
+print(0o14) 
+print(0xC) 
+print(int(False))
+print(int(True))
---
+++ ch2/str.py
@@ -0,0 +1,9 @@
+print('python')
+print("3.8")
+print("It's a beautiful day!")
+print('"HI!" says Jack')
+a= "0123456789"
+print(a[0])
+print(a[1])
+print('python'[0])
+print('python'[1])
